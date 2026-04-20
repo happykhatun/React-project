0297a00c3cf49886ea5f731dc61f7e4592069bb9
happykhatun <ajmiyarahappy@gmail.com>
2026-04-20 18:42:19 +0600

    navbar update with responsive menu

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,10 @@
 import Logo from '../assets/Logo.png';
 import Arrow_Down from '../assets/Arrow_Down.svg';
+import { MdMenu } from "react-icons/md";
+import { useState } from 'react';
 
 const NavBar = () => {
+    const [open, setOpen] = useState(false)
     return (
         <>
             <nav className='py-6 bg-[#FBF3F0]'>
@@ -10,15 +13,35 @@ const NavBar = () => {
                         <div>
                             <img src={Logo} alt="logo" />
                         </div>
-                        <ul className='flex gap-12 text-[18px] font-semibold text-[#29313D]'>
+                        <ul className=' hidden md:flex gap-12 text-[18px] font-semibold text-[#29313D]'>
                             <li className='flex items-center gap-2'><a href="">Home</a> <span><img src={Arrow_Down} alt="" /></span> </li>
                             <li className='flex items-center gap-2'><a href="">About</a> <span><img src={Arrow_Down} alt="" /></span></li>
                             <li className='flex items-center gap-2'><a href="">Services</a> <span><img src={Arrow_Down} alt="" /></span></li>
                             <li className='flex items-center gap-2'><a href="">works</a> <span><img src={Arrow_Down} alt="" /></span></li>
                             <li className='flex items-center gap-2'><a href="">Blog</a><span><img src={Arrow_Down} alt="" /></span></li>
                         </ul>
-                        <button className='text-[18px] font-semibold text-[#FF5538] py-3.5 px-7 rounded-[18px] border border-[#FF5538]'>Contact us</button>
+                        <button className=' hidden md:block text-[18px] font-semibold text-[#FF5538] py-3.5 px-7 rounded-[18px] border border-[#FF5538]'>Contact us</button>
+
+
+                        <div className='md:hidden cursor-pointer' onClick={() => setOpen(!open)}>
+                            <MdMenu />
+                        </div>
                     </div>
+                    {
+                        open && (
+                            <div className='md:hidden'>
+                                <ul className='flex flex-col gap-8 text-[18px] font-semibold text-[#29313D]'>
+                                    <li ><a href="">Home</a> </li>
+                                    <li ><a href="">About</a></li>
+                                    <li><a href="">Services</a></li>
+                                    <li><a href="">works</a></li>
+                                    <li ><a href="">Blog</a></li>
+                                </ul>
+                                <button className=' text-[18px] font-semibold text-[#FF5538] py-3.5 px-7 rounded-[18px] border border-[#FF5538]'>Contact us</button>
+                            </div>
+
+                        )
+                    }
                 </div>
             </nav>
 
